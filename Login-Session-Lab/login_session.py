ac3d9a8faaf542c25634a64c84a94327c1032720
yazanAz20-meet <yazan7azaizah@gmail.com>
2023-07-21 14:08:15 +0300

    finish
    but still I have the bonus

--- Login-Session-Lab/login_session.py
+++ Login-Session-Lab/login_session.py
@@ -4,9 +4,16 @@ from flask import session as login_session
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super-secret-key'
 
-@app.route('/', ) # What methods are needed?
+@app.route('/', methods = ['POST','GET'] ) # What methods are needed?
 def home():
-	
+	if request.method == 'POST':
+		try:
+			login_session['author'] = request.form['author']
+			login_session['quote'] = request.form['quote']
+			login_session['age'] = request.form['age']
+			return redirect(url_for('thanks'))
+		except:
+			return redirect(url_for('error'))
 	return render_template('home.html')
 
 
@@ -19,7 +26,7 @@ def error():
 @app.route('/display')
 def display():
 
-	return render_template('display.html', ) # What variables are needed?
+	return render_template('display.html', author = login_session['author'], quote = login_session['quote'] , age = login_session['age']) # What variables are needed?
 
 
 @app.route('/thanks')
